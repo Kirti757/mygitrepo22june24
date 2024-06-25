8c455e3e27801a25d3805a8b050442482dae48a8
Kirti757 <kaderukirti@gmail.com>
2024-06-25 16:26:47 +0530

    this commit by main

--- f3.java
+++ f3.java
@@ -1,8 +1,8 @@
-<<<<<<< Updated upstream
-cComplete manager work
-=======
+
+Complete manager work
 This is 1st line of code
 This is 2nd line of code
 This is 3rd line of code
 This is 4th line of code
->>>>>>> Stashed changes
+
+This line is by main
